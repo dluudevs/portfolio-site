c6c0b8e7dfc8a3ad12aa907505dfdb5bbd26853f
Derek <derekluu90@gmail.com>
2019-03-20 07:23:45 -0400

    media queries to get to tablet viewport

--- scripts/script.js
+++ scripts/script.js
@@ -1,55 +1,65 @@
-$(document).ready(function () {
+$(document).ready( () => {
 
-    //fixed header after scrolling past the header
-    $(window).on('scroll', () => {
-        if($(this).scrollTop() >= $(window).height() - $('nav').height() ){
-            $('nav').removeClass('nav_absolute').addClass('nav_fixed');
-        } else { 
-            $('nav').removeClass('nav_fixed').addClass('nav_absolute');
-        }
-    })
-
-    $(window).on('scroll', () => {
-        if($(this).scrollTop() > 0){
-            // $('nav').css({
-            //     'box-shadow': '0 5px 5px 0.6'
-            // });
-            $('nav').addClass('nav_scrolled');
-        } else {
-            $('nav').removeClass('nav_scrolled');
-        }
-    })
+    toggleMobileMenu();
+    
+    scrollThenFixed();
 
     AOS.init({
         duration: 1500,
         once: true,
     });
 
-    $('.hamburger').on('click', function () {
-        $(this).toggleClass('is_active');
-        toggleMobileMenu();
-    })
-
     $('a').smoothScroll({
         easing: 'swing',
         speed: 1250
-    })
+    });
 
-    $('.nav_list a').on('click', () => {
-        $('.hamburger').removeClass('is_active');
-        $('.nav_mobile').transition({ x: '190%', opacity: 0 }, 500, () => {
-            $('.nav_list').removeClass('nav_mobile');
-        });
-    })
-})
+});
 
 const toggleMobileMenu = () => {
-    if (!$('.nav_list').hasClass('nav_mobile')) {
-        $('.nav_list').addClass('nav_mobile');
-        $('.nav_mobile').transition({ x: '92.5%', opacity: 1 }, 500);
-    } else if ($('.nav_list').hasClass('nav_mobile')) {
-        $('.nav_mobile').transition({ x: '190%', opacity: 0 }, 500, () => {
-            $('.nav_list').removeClass('nav_mobile');
-        });
-    }
+
+    $('.hamburger').on('click', () => {
+
+        //animate transition to X on click
+        $(this).toggleClass('is_active');
+        
+        //animation for hamburger menu
+        if (!$('.nav_list').hasClass('nav_mobile')) {
+            $('.nav_list').addClass('nav_mobile');
+            $('.nav_mobile').transition({ x: '92.5%', opacity: 1 }, 500);
+        } else if ($('.nav_list').hasClass('nav_mobile')) {
+            $('.nav_mobile').transition({ x: '190%', opacity: 0 }, 500, () => {
+                $('.nav_list').removeClass('nav_mobile');
+            });
+        }
+
+        //switch to hamburger after clicking a link in the menu
+        $('.nav_list a').on('click', () => {
+            $('.hamburger').removeClass('is_active');
+            $('.nav_mobile').transition({ x: '190%', opacity: 0 }, 500, () => {
+                $('.nav_list').removeClass('nav_mobile');
+            });
+        })
+    })
+}
+
+const scrollThenFixed = () => {
+
+    //fixed scrollbar
+    $(window).on('scroll', () => {
+        if ($(this).scrollTop() >= $(window).height() - $('nav').height()) {
+            $('nav').removeClass('nav_absolute').addClass('nav_fixed');
+        } else {
+            $('nav').removeClass('nav_fixed').addClass('nav_absolute');
+        }
+    })
+
+    //box shadow
+    $(window).on('scroll', () => {
+        if ($(this).scrollTop() > 0) {
+            $('nav').addClass('nav_scrolled');
+        } else {
+            $('nav').removeClass('nav_scrolled');
+        }
+    })
 }
